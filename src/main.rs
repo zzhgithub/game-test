550dbf2551059b4d9f2c37a03540a0db3c34ea95
zhouzihao <1042181618@qq.com>
2022-08-13 23:15:12 +0800

    dev

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,66 @@ fn main() {
         .add_plugin(PlayerPlugin)
         .add_startup_system(setup)
         .add_system(dynamic_load_system)
+        .add_system(dynamic_render_system)
         .run();
 }
 
 /**
  * todo 动态绘制系统
  * 当 cube added的时候 和 cube 的状态改变时 才重新 绘制！
+ * 注意这是一段测试代码
  */
-fn dynamic_render_system() {}
+fn dynamic_render_system(
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut commands: Commands,
+    query: Query<(Entity, &Transform), Added<Cube>>,
+) {
+    const CUBLE_SIZE: f32 = 1.0;
+    let texture_handle: Handle<Image> = asset_server.load("a.jpeg");
+    // 声明一个 2D 的贴图
+    let quad_handle = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
+        CUBLE_SIZE, CUBLE_SIZE,
+    ))));
+    // 使用图片生成一种文理
+    let material_handle = materials.add(StandardMaterial {
+        base_color_texture: Some(texture_handle.clone()),
+        alpha_mode: AlphaMode::Blend,
+        unlit: true,
+        ..default()
+    });
+
+    // 查询新增的 组件
+    for (ele, transform) in query.iter() {
+        // 这里 是测试代码 如果组件的 y 小于等于0就进行渲染
+        // info!("checked");
+        if transform.translation.y == 0.0 {
+            commands.entity(ele).insert_bundle(PbrBundle {
+                mesh: quad_handle.clone(),
+                material: material_handle.clone(),
+                transform: get_transform_by_face_type(
+                    FaceType::Up,
+                    transform.clone(),
+                    // Transform::from_xyz(0.0, 0.0, 0.0),
+                    CUBLE_SIZE,
+                ),
+                ..Default::default()
+            });
+            // .add_children(|childern| {
+            //     childern.spawn_bundle(PbrBundle {
+            //         mesh: quad_handle.clone(),
+            //         material: material_handle.clone(),
+            //         transform: get_transform_by_face_type(
+            //             FaceType::Up,
+            //             Transform::from_xyz(0.0, 0.0, 0.0),
+            //             CUBLE_SIZE,
+            //         ),
+            //         ..Default::default()
+            //     });
+        }
+    }
+}
 
 /**
  * 动态加载数据
@@ -49,11 +101,59 @@ fn dynamic_load_system(
         let may_z = at.z.round();
         // 数据每一帧都在加载 是不行的
         // 必选它走到边缘的时候才加载！！！
-        let load_size: f32 = 15.0;
+        let load_size: f32 = 5.0;
         let check_size: f32 = 5.0;
         // todo 八个方向 中 如果发现 5格子内没有了才进行加载
         // fixme 先处理 要不加载的数据 可以先进行清除? 全部清除掉 其中有的 CloseTo 组件？
-        if !data.contains_key(&Point3D::new(may_x as i32, may_y as i32, may_z as i32)) {
+        if !data.contains_key(&Point3D::new(may_x as i32, may_y as i32, may_z as i32))
+            || !data.contains_key(&Point3D::new(
+                (may_x + check_size) as i32,
+                may_y as i32,
+                may_z as i32,
+            ))
+            || !data.contains_key(&Point3D::new(
+                (may_x - check_size) as i32,
+                may_y as i32,
+                may_z as i32,
+            ))
+            || !data.contains_key(&Point3D::new(
+                may_x as i32,
+                (may_y + check_size) as i32,
+                may_z as i32,
+            ))
+            || !data.contains_key(&Point3D::new(
+                may_x as i32,
+                (may_y - check_size) as i32,
+                may_z as i32,
+            ))
+            || !data.contains_key(&Point3D::new(
+                may_x as i32,
+                may_y as i32,
+                (may_z + check_size) as i32,
+            ))
+            || !data.contains_key(&Point3D::new(
+                may_x as i32,
+                may_y as i32,
+                (may_z + check_size) as i32,
+            ))
+        {
+            info!("clean");
+            // 查询过 远的数据 并且清除
+            let mut to_remove = Vec::new();
+            for (point3d, entity) in &*data {
+                let sum = (may_x - point3d.x as f32).powi(2)
+                    + (may_y - point3d.y as f32).powi(2)
+                    + (may_z - point3d.z as f32).powi(2);
+                if sum.sqrt() > ((15.0 as f32).powi(2) * 2.0 as f32).sqrt() {
+                    to_remove.push(point3d.to_owned());
+                    commands.entity(entity.to_owned()).despawn();
+                }
+            }
+            for key in to_remove.iter() {
+                data.remove(key);
+            }
+
+            info!("loading");
             // ! 加载数据
             for x in (may_x - load_size) as i32..(may_x + load_size) as i32 {
                 for y in (may_y - load_size) as i32..(may_y + load_size) as i32 {
@@ -63,18 +163,21 @@ fn dynamic_load_system(
                         match data.get(&check_point) {
                             Some(e) => {
                                 entity = e.to_owned();
-                                info!("已经加载了{:?}", check_point);
+                                // info!("已经加载了{:?}", check_point);
                             }
                             None => {
                                 // 这里要判断一下这里的 block 是否要进入到这里面
-                                // !!
                                 // 如果不存在的情况下 创建这个 对象
-                                entity = commands.spawn().insert(Cube).id();
+                                entity = commands
+                                    .spawn()
+                                    .insert(Transform::from_xyz(x as f32, y as f32, z as f32))
+                                    .insert(Cube)
+                                    .id();
                                 // 把这个新的 缓存进去
                                 data.insert(check_point, entity);
                                 // 这里 应该根据 某种方法来判断 里面的值? 通过一个点 来加载里面的信息
                                 // todo load_cube_by_point3d 方法查询 这个点 应该算出来的结果~~
-                                info!("正在加载{:?}", check_point);
+                                // info!("正在加载{:?}", check_point);
                                 // 然后添加一个偏移量
                             }
                         }
